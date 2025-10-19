005911a5bf00e326db8c698fd3c32a181da12a12
Rian Finnegan <rian.finnegan@orkestra.energy>
2025-10-19 10:05:53 +0800

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,17 @@
 import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://thegoldenhourco.net', // Update this with your actual domain
   adapter: cloudflare({
     platformProxy: {
       enabled: true
     },
 
     imageService: "cloudflare"
-  })
+  }),
+  integrations: [sitemap()]
 });
